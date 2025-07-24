c48d58815d56d7154c50f0a7679d4b845404ae3e
Tiger Hou <44105132+TigerHou2@users.noreply.github.com>
2025-07-24 07:56:38 -0400

    Use sphinx for documentation
    Easier to auto generate docs for source + better jupyter integration

--- STLib/sources.py
+++ STLib/sources.py
@@ -3,9 +3,9 @@ import os.path
 import csv
 
 from typing import Union
-from numpy.typing import ArrayLike
 from astropy import units
 from astropy.time import Time
+from astropy.table import Column
 from .utils import type_checker, Number, horizonsQuery, naifSchemaToExtended, naifSchemaToOriginal
 from . import \
     DEFAULT_SMALL_BODIES_PATH, DEFAULT_CACHE_EXPIRATION_DAYS, \
@@ -17,65 +17,129 @@ import spiceypy as spice
 
 class AstronomicalSources:
 
+    """Astronomical sources base class.
+
+    This is the base class for :class:`~STLib.sources.SpectralSources` and :class:`~STLib.sources.MagnitudeSources`.
+
+    Parameters
+    ----------
+    ra: (N, ) `astropy.units.Quantity` ['angle'], optional
+        Right ascension of the sources(s).
+    dec: (N, ) :class:`~astropy.units.Quantity` ['angle'], optional
+        Declination of the sources(s).
+    source_id: (N, ) :class:`list` | :class:`~astropy.table.Column`, optional
+        Corresponding source IDs (e.g. from input catalogs).
+    """
+
     @type_checker
     def __init__(self, *,
-                 ra : Union[ArrayLike, None],
-                 dec: Union[ArrayLike, None],
-                 source_id: Union[ArrayLike, None]):
+                 ra : units.Quantity = np.array([]) * units.degree,
+                 dec: units.Quantity = np.array([]) * units.degree,
+                 source_id: Union[list, Column] = []):
         
-        '''
-        Base class for astronomical sources. 
-        '''
-        if ra is None or dec is None or source_id is None:
-            self.ra  = np.array([]) * units.degree
-            self.dec = np.array([]) * units.degree
-            self.source_id = []
-            return
+        """Constructor for the AstronomicalSources class.
+        """
 
-        self.ra = ra.to(units.degree)
-        self.dec = dec.to(units.degree)
+        self.ra = np.atleast_1d(ra).to(units.degree)
+        self.dec = np.atleast_1d(dec).to(units.degree)
         self.source_id = source_id
 
 
 class SpectralSources(AstronomicalSources):
 
+    """Astronomical sources defined by their spectral flux density profiles.
+
+    Spectral flux density is the energy per area per unit wavelength. Integrating the product of spectral flux density
+    with the transmission curve of a given filter allows for accurate computation of the measured flux.
+
+    Parameters
+    ----------
+    ra: (N, ) :class:`~astropy.units.Quantity` ['angle']
+        Right ascension of the sources(s).
+    dec: (N, ) :class:`~astropy.units.Quantity` ['angle']
+        Declination of the sources(s).
+    source_id: (N, ) Union[list, :class:`~astropy.table.Column`]
+        Corresponding source IDs (e.g. from input catalogs).
+    sfd_wavelength: (M, ) :class:`~astropy.units.Quantity` ['length']
+        Spectral flux density sample points.
+    spectral_flux_density: (N, M) :class:`~astropy.units.Quantity` ['spectral flux density']
+        Spectral flux density values corresponding to each sample point. Can be either:
+
+        - irradiance / wavelength (e.g., :class:`~astropy.units.Jy`), or
+        - irradiance / frequency (e.g., :class:`~astropy.units.W`/:class:`~astropy.units.m**2`/:class:`~astropy.units.nm`)
+    """
+
     @type_checker
     def __init__(self, *,
-                 ra : ArrayLike,
-                 dec: ArrayLike,
-                 source_id: ArrayLike,
-                 spectral_flux_density  : ArrayLike,
-                 sfd_wavelength         : ArrayLike):
+                 ra : units.Quantity,
+                 dec: units.Quantity,
+                 source_id: Union[list, Column],
+                 spectral_flux_density  : units.Quantity,
+                 sfd_wavelength         : units.Quantity):
         
-        '''
-        Define a collection of sources (stars, planetary bodies, etc.) by their right ascension, declination, id,
-            and a list of spectral flux densities and wavelength arrays defining the flux profile of each source:
-                - spectral flux density (energy ⋅ time⁻¹ ⋅ area⁻¹ ⋅ wavelength⁻¹)
-                - spectral flux density sample points (wavelength)
-        '''
+        """Constructor for the SpectralSources class.
+        """
+
         super().__init__(ra=ra, dec=dec, source_id=source_id)
-        self.spectral_flux_density = spectral_flux_density.to(units.W / units.nm / units.m**2)
+        self.spectral_flux_density = np.atleast_2d(spectral_flux_density).to(units.W / units.nm / units.m**2)
         self.sfd_wavelength = sfd_wavelength.to(units.nm)
 
 
 class MagnitudeSources(AstronomicalSources):
 
+    """ Astronomical sources defined by their magnitude.
+
+    Magnitude is defined with respect to a given filter and zero point flux. It is not possible to determine the
+    measured flux of a source with a different filter than the one used to define its magnitude. This class should
+    only be used when a source's spectral flux density is not available, and only with a matching filter + zero 
+    point flux (see :class:`~STLib.filter.Filter`).
+
+    Parameters
+    ----------
+    ra: (N, ) `~astropy.units.Quantity` ['angle']
+        Right ascension of the sources(s).
+    dec: (N, ) `~astropy.units.Quantity` ['angle']
+        Declination of the sources(s).
+    source_id: (N, ) array_like
+        Corresponding source IDs (e.g. from input catalogs).
+    magnitude: (N, ) :class:`~numpy.ndarray`
+        Magnitudes of the source(s).
+    """
+
     @type_checker
     def __init__(self, *,
-                 ra : ArrayLike,
-                 dec: ArrayLike,
-                 source_id: ArrayLike,
-                 magnitude: ArrayLike):
+                 ra : units.Quantity,
+                 dec: units.Quantity,
+                 source_id: Union[list, Column],
+                 magnitude: np.ndarray):
         
-        '''
-        Define a collection of sources (stars, planetary bodies, etc.) by their right ascension, declination, id,
-            and magnitude.
-        '''
+        """Constructor for the MagnitudeSources class.
+        """
+
         super().__init__(ra=ra, dec=dec, source_id=source_id)
         self.magnitude = magnitude
 
 
 class SolarSystemSources:
+
+    """Solar System sources, e.g., asteroids. 
+    
+    Defined by a file containing JPL SBDB query results and desired ephemeris start/end dates. Upon instantiation, 
+    queries the JPL Small Body Database for SPICE kernels corresponding to the small bodies listed in the query for
+    the coverage defined by the start and end dates. Cached kernels that do not exceed the max cache age and with
+    sufficient coverage are reused.
+
+    Parameters
+    ----------
+    start_date: :class:`~astropy.time.Time`
+        Start date of the ephemeris.
+    end_date: :class:`~astropy.time.Time`
+        End date of the ephemeris.
+    small_bodies_query_results: :class:`str`
+        Path to the SBDB query results.
+    max_cache_age: :class:`float`
+        Maximum age of the downloaded kernels in days.
+    """
 
     @type_checker
     def __init__(self, *,
@@ -84,11 +148,8 @@ class SolarSystemSources:
                  small_bodies_query_results: str = DEFAULT_SMALL_BODIES_PATH,
                  max_cache_age: Number = DEFAULT_CACHE_EXPIRATION_DAYS):
         
-        '''
-        Define a collection of small bodies in the Solar System by:
-        - a path to the SBDB query results obtained from `STLib.utils.sbdbQuery`, and
-        - a list of SPICE kernels obtained from `STLib.utils.horizonsQuery`
-        '''
+        """Constructor for the SolarSystemSources class.
+        """
 
         # use the results of an existing SBDB query to download the appropriate small body SPICE kernels
         with open(small_bodies_query_results, 'r') as f:
@@ -159,6 +220,18 @@ class SolarSystemSources:
 
 
     def update(self, observer_id, time: Time):
+
+        """
+        Update the state of each source as seen by an observer at a given time. Includes relative position, velocity,
+        right ascension, declination, and magnitude.
+
+        Parameters
+        ----------
+        observer_id: str
+            NAIF ID of the observer.
+        time: :class:`~astropy.time.Time`
+            Time of observation.
+        """
         
         spice.furnsh([LEAPSECONDS_KERNEL, DE_KERNEL])
 
@@ -187,6 +260,21 @@ class SolarSystemSources:
 
 
 def _readSmassSpectra(fname):
+    """
+    Reads a SMASS spectrum file and returns the wavelengths and normalized fluxes.
+
+    Parameters
+    ----------
+    fname : str
+        path to the SMASS spectrum file
+
+    Returns
+    -------
+    wavelengths : :class:`~astropy.units.Quantity`
+        an array of wavelengths in nanometers
+    normalized_flux : :class:`~numpy.ndarray`
+        an array of normalized fluxes
+    """
     with open(fname, 'r') as f:
         reader = csv.reader(f, delimiter=' ')
         wavelengths = []
@@ -200,6 +288,22 @@ def _readSmassSpectra(fname):
 
 
 def _readMeanSpectra(spec):
+    """
+    Reads a mean spectrum file for a given spectral class and returns the wavelengths and normalized fluxes.
+
+    Parameters
+    ----------
+    spec : str
+        A string representing the spectral class to be used for fetching the respective data column.
+
+    Returns
+    -------
+    wavelengths : :class:`~astropy.units.Quantity`
+        An array of wavelengths in nanometers.
+    normalized_flux : :class:`~numpy.ndarray`
+        An array of normalized fluxes for the specified spectral class.
+    """
+
     with open(SMALL_BODIES_MEAN_SPECTRA_PATH, 'r') as f:
         reader = csv.reader(f, delimiter=' ')
         wavelengths = []
@@ -255,11 +359,47 @@ def _readMeanSpectra(spec):
 
 
 def _fnorm3(a):
+    """
+    Calculate the Euclidean norm of a 3-element vector.
+
+    Parameters
+    ----------
+    a : array_like
+        3-element vector
+
+    Returns
+    -------
+    norm : float
+        Euclidean norm of the vector
+    """
     return np.sqrt(a[0]**2 + a[1]**2 + a[2]**2)
 
 
 def _q(alpha, G):
     # https://en.wikipedia.org/wiki/Absolute_magnitude#Asteroids
+    """
+    Calculate the phase integral q(alpha, G) used in the HG12 system.
+
+    Parameters
+    ----------
+    alpha : float or array_like
+        Phase angle in degrees
+    G : float
+        Slope parameter
+
+    Returns
+    -------
+    q : float or array_like
+        Phase integral
+
+    Notes
+    -----
+    The phase integral q(alpha, G) is defined as:
+        q(alpha, G) = (1-G) * exp(-A1 * tan(alpha/2)**B1) \
+                    + G * exp(-A2 * tan(alpha/2)**B2)
+    where A1, A2, B1, and B2 are constants.  The phase integral is
+    undefined for alpha > 120.0 degrees.
+    """
     A1 = 3.332
     A2 = 1.862
     B1 = 0.631
